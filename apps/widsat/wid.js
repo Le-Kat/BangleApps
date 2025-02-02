834d8d8666fa7e7a1df4f638a8d39b750ae007b0
Le~Kat <Le3Kat@protonmail.com>
2025-02-02 18:47:14 -0500

    draw an icon to screen

--- apps/widsat/wid.js
+++ apps/widsat/wid.js
@@ -1,4 +1,14 @@
 (() => {
 	
-	
+	WIDGETS["mywidget"]={
+		area:"tl",
+		width: 24,
+		draw:function() {
+			g.reset().drawImage (
+				atob ("GBiBAAAAAAAAPASSfgSSTgSSBgSSBgSTDATZgAZMwAJmfAMzAAEZgAGM/ADGAABjgAAw/AAcAAAHAAAB/AAAAAAAAAAAAAAAAAAAAA=="),
+				this.x,
+				this.y
+			);
+		}
+	};
 })()
