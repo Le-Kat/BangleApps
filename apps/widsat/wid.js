bf358439d0a7b7a828ce2c37b5c67cea04aece49
Le~Kat <Le3Kat@protonmail.com>
2025-02-02 19:20:25 -0500

    basic satelite counting functionality

--- apps/widsat/wid.js
+++ apps/widsat/wid.js
@@ -1,14 +1,54 @@
 (() => {
 	
+	const SATELITES = [
+		// none
+		atob ("GBiBAAAAAAAAPAAAfgAATgAABgAABgAADAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=="),
+		// one
+		atob ("GBiBAAAAAAAAPAACfgACTgACBgACBgADDAABgAAAwAAAfAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=="),
+		// two
+		atob ("GBiBAAAAAAAAPAASfgASTgASBgASBgATDAAZgAAMwAAGfAADAAABgAAA/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=="),
+		// three
+		atob ("GBiBAAAAAAAAPACSfgCSTgCSBgCSBgCTDADZgABMwABmfAAzAAAZgAAM/AAGAAADgAAA/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=="),
+		// four +
+		atob ("GBiBAAAAAAAAPASSfgSSTgSSBgSSBgSTDATZgAZMwAJmfAMzAAEZgAGM/ADGAABjgAAw/AAcAAAHAAAB/AAAAAAAAAAAAAAAAAAAAA==")
+	];
+	
 	WIDGETS["mywidget"]={
 		area:"tl",
-		width: 24,
+		width: Bangle.isGPSOn() ? 24 : 0,
 		draw:function() {
-			g.reset().drawImage (
-				atob ("GBiBAAAAAAAAPASSfgSSDgSSBgSSBgSTBATZgAZMwAJmfAMzAAEZgAGM/ADGAABjgAAw/AAcAAAHAAAB/AAAAAAAAAAAAAAAAAAAAA=="),
-				this.x,
-				this.y
-			);
+			
+			var gps = Bangle.getGPSFix();
+			if (gps !== undefined) {
+				
+				var s = gps ["satellites"];
+				if (s !== undefined) {
+					
+					var img;
+					if (s == 0) {
+						
+						img = SATELITES [0];
+					} else if (s == 1) {
+						
+						img = SATELITES [1];
+					} else if (s == 2) {
+						
+						img = SATELITES [2];
+					} else if (s == 3) {
+						
+						img = SATELITES [3];
+					} else {
+						
+						img = SATELITES [4];
+					}
+					
+					g.reset().drawImage (
+						img,
+						this.x,
+						this.y
+					);
+				}
+			}
 		}
 	};
 })()
