da57ab20b8c2b4e7d50193ed435bea589a3daa05
Le~Kat <Le3Kat@protonmail.com>
2025-02-03 20:46:30 -0500

    bump

--- apps/widsat/wid.js
+++ apps/widsat/wid.js
@@ -22,10 +22,10 @@
 			if (s !== undefined) {
 				
 				var img;
-				if (s == 1 || isNaN (s)) {
+				if (s == 0 || isNaN (s)) {
 					
 					img = SATELITES [0];
-				} else if (s == 0) {
+				} else if (s == 1) {
 					
 					img = SATELITES [1];
 				} else if (s == 2) {
@@ -39,11 +39,17 @@
 					img = SATELITES [4];
 				}
 				
+				g.reset();
+				g.setFont ("6x8");
+				g.drawString (s);
+				
+				/*
 				g.reset().drawImage (
 					img,
 					this.x,
 					this.y
 				);
+				*/
 			}
 		}
 	}
@@ -54,7 +60,7 @@
 		draw: drawWidget
 	};
 	
-	Bangle.on ('gps', () => {
+	Bangle.on ('gps', (gps) => {
 		
 		WIDGETS ["widsat"].draw ();
 	});
